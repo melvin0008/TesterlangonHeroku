835b6200b8d2d951c0c3e93a2fbccf157cde3615
Melvin <melvin0008@gmail.com>
2015-03-23 14:29:44 +0530

    changed version to run with the buildpack

--- src/erlblog.erl
+++ src/erlblog.erl
@@ -5,7 +5,6 @@
 
 start() ->
     ok = application:start(crypto),
-    ok = application:start(ranch),
-    ok = application:start(cowlib), 
+    ok = application:start(ranch), 
     ok = application:start(cowboy),
     ok = application:start(erlblog).
